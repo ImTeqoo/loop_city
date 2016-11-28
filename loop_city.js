6c942316a5dddafaee07df09e4ae4e909e9622c5
Talon Navarez <navareztalon@gmail.com>
2016-11-28 08:46:21 -1000

    pizza

---
+++ loop_city.js
@@ -0,0 +1,24 @@
+// looper duper city grouper//
+
+// 1st loop //
+var count = 0;
+
+for(var count = 0; count <= 100; count ++){
+  console.log("talon can count to" + count);
+}
+
+// 2nd loop //
+var animalcount = 0;
+
+for(var animalcount = 0; animalcount <= 5; animalcount ++){
+	console.log("there are" + animalcount + "many animals in Noah's arc");
+}
+
+// 3rd loop //
+var partyanimals = (bill, tom, john, eric, craig);
+var arraylength = partyanimals.length;
+
+for(var i = 0; i < arraylength; i++){
+	alert(mystringarray[i]);
+	console.log(bill + "WHAT ARE YOU DOING!?")
+}
